109bad6cc72f03abdb789c361602e6b3d39ab9ff
vik_orel <vikorel34@gmail.com>
2021-12-11 14:45:46 +0200

    add hero slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './sass/main.scss';
+// ===============================================
+const Refs = {
+  slider: document.querySelector('.swiper'),
+  nextSlide: document.querySelector('.swiper-button-next'),
+  prevSlide: document.querySelector('.swiper-button-prev'),
+};
+// ===============================================
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
